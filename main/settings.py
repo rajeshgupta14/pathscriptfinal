876a687e37a76d19d961e2b6bee120c10071bbec
Rajesh Gupta <gupta.rajesh1996@gmail.com>
2017-12-13 11:51:30 +0530

    second commit

--- main/settings.py
+++ main/settings.py
@@ -31,7 +31,7 @@ DEBUG = bool( os.environ.get('DJANGO_DEBUG',True) )
 
 
 ALLOWED_HOSTS = [
-        'quiet-hamlet-51594.herokuapp.com',
+        'hidden-dusk-76878.herokuapp.com',
         '127.0.0.1',
         ]
 
