ccb398271b9c8e1871c4ec08864e3e68067483e8
Rajesh Gupta <gupta.rajesh1996@gmail.com>
2017-12-23 19:43:15 +0530

    Small change

--- myapp/views.py
+++ myapp/views.py
@@ -12,29 +12,29 @@ from django.contrib.auth.decorators import login_required
 
 
 
-@login_required 
+@login_required
 def clients(request):
     curr=request.user
     return render_to_response('clients.html',{'user':curr,'clients':Client.objects.filter(userid=curr.id)})
 
 @csrf_protect
 def auth_view(request):
-    next = request.GET.get('next','/home/')
     if request.method == 'POST':
      username = request.POST.get('username','')
      password = request.POST.get('password','')
      user = authenticate(username=username,password=password)
-     
+
      if user is not None:
       if user.is_active:
        login(request, user)
+       next = request.GET.get('next','/home/')
        return HttpResponseRedirect(next)
       else:
        HttpResponse("Inactive User")
      else:
       return HttpResponseRedirect(settings.LOGIN_URL)
     return render(request,'login.html',{'redirect_to':next})
-    
+
 
 
 @login_required
@@ -45,7 +45,7 @@ def home(request):
      #return render(request,'clients.html',{'clients':Client.objects.filter(userid=curr.id)})
     else:
      return render(request,'dummy.html',{'clients':Client.objects.filter(clientname=curr.clientid)})
- 
+
 def invalid_login(request):
     return render_to_response('invalid_login.html')
 
